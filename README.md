60672bd5fe8cdbf8f6801965bdeddf0389b1b6cb
Toluwashe Ajise <digittolu25@gmail.com>
2021-05-12 17:12:53 +0100

    add readme file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Wherefore-art-thou-freeCodeCamp
+
+
+My attempt on the freeCodeCamp Wherefore art thou Challenge
+
+[Challenge can be found here](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/wherefore-art-thou)
+
---
+++ index.js
@@ -0,0 +1,10 @@
+function whatIsInAName(collection, source) {
+    var arr = [];
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+    return arr;
+  }
+  
+  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
